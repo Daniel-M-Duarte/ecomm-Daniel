5562daf166f47fa70b6dd01b6a7110aee5fdd2c3
Daniel-M-Duarte <danielmacield@gmail.com>
2023-02-04 16:54:30 -0300

    feat: CRUD Products

--- product/api/src/routes/index.js
+++ product/api/src/routes/index.js
@@ -1,11 +1,14 @@
 import express  from "express";
 import app from "../app.js";
 import categories from "./categoriesRoutes.js";
+import products from "./productsRoutes.js"
+
 
 const routes = (app) =>{
     app.use(
         express.json(),
-        categories
+        categories, 
+        products
     )
 }
 
---
+++ product/api/src/routes/productsRoutes.js
@@ -0,0 +1,14 @@
+// productsRoutes
+import express from 'express';
+import ProductController from '../controllers/productsControlle.js';
+
+const router = express.Router();
+
+router
+  .get("/api/products", ProductController.listarProdutos)
+  .get("/api/products/:id", ProductController.listarProdutosById)
+  .post("/api/admin/products", ProductController.addNovoProduto)
+  .put("/api/admin/products/:id", ProductController.atualizaProduto)
+  .delete("/api/admin/products/:id", ProductController.apagaProduto)
+  
+export default router;  
