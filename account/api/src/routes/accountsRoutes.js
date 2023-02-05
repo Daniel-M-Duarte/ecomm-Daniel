c71b823282295a6e844fd72a54ee391523f19c65
Daniel-M-Duarte <danielmacield@gmail.com>
2023-02-05 13:04:55 -0300

    feat: CRUD of accounts add

---
+++ account/api/src/routes/accountsRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import AccountController from '../controllers/accountsControlle.js';
+
+const router = express.Router();
+
+router
+  .get("/api/accounts", AccountController.listarContas)
+  .get("/api/accounts/:id", AccountController.buscarContabyId)
+  .post("/api/admin/accounts", AccountController.inserirConta)
+  .delete("/api/admin/accounts/:id", AccountController.deletarConta)
+  .put("/api/admin/accounts/:id", AccountController.atualizatConta)
+
+export default router;  
