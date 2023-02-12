66393b0f3b5c73593943d7a81cb6f1b8d16c683b
Daniel-M-Duarte <danielmacield@gmail.com>
2023-02-12 15:47:50 -0300

    fix: Changing port number to: 3006 and 3007

--- account/server.js
+++ account/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import app from "./src/app.js";
 
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3006;
 
 app.listen(port, ()=>{
     console.log(`Servidor escutando em http://localhost:${port}`)
--- product/server.js
+++ product/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import app from './src/app.js';
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3007;
 
 app.listen(port, () =>{
     console.log(`Servidor escutando em http://localhost:${port}`)
