050e6258356b28bd723e7268f3372d523199878a
Daniel-M-Duarte <danielmacield@gmail.com>
2023-02-12 16:00:47 -0300

    fix: Error handling add in accounts

--- account/src/controllers/accountsControlle.js
+++ account/src/controllers/accountsControlle.js
@@ -4,6 +4,7 @@ class AccountController{
 
   static listarContas = (req, res) =>{
     users.find((err, users) =>{
+        if(err) return res.status(500).json({message: `${err.message}`})
         res.status(200).json(users)
         })
     }
