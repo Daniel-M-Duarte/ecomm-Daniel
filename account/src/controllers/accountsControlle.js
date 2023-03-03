05a63d85d050d9f7e4441eb7a7c25e3e3a3dec0f
Daniel-M-Duarte <danielmacield@gmail.com>
2023-03-03 16:55:26 -0300

    fix: eslint in accounts

--- account/src/controllers/accountsControlle.js
+++ account/src/controllers/accountsControlle.js
@@ -1,34 +1,34 @@
-// eslint-disable-next-line import/extensions
+/* eslint-disable no-shadow */
+/* eslint-disable new-cap */
 import users from '../models/Account.js';
 
 class AccountController {
   static listarContas = (req, res) => {
-    users.find((err, users) => {
-      res.status(200).json(users);
-    });
+    users.find((err, users) => res.status(200).json(users));
   };
 
   static inserirConta = (req, res) => {
     const user = new users(req.body);
     user.save((err) => {
       if (err) return res.status(500).json({ message: `${err.message} - FALHA AO CADASTRAR` });
-      res.status(201).json(user);
+      return res.status(201).json(user);
     });
   };
 
   static buscarContabyId = (req, res) => {
     const { id } = req.params;
+    // eslint-disable-next-line no-shadow
     users.findById(id, (err, users) => {
       if (err) return res.status(404).send('ID not found');
-      res.status(200).json(users);
+      return res.status(200).json(users);
     });
   };
 
   static deletarConta = (req, res) => {
     const { id } = req.params;
-    users.findByIdAndDelete(id, (err, users) => {
+    users.findByIdAndDelete(id, (err) => {
       if (err) return res.status(404).send('ID not found');
-      res.status(204).json({ message: 'Excluido com sucesso' });
+      return res.status(204).json({ message: 'Excluido com sucesso' });
     });
   };
 
@@ -36,7 +36,7 @@ class AccountController {
     const { id } = req.params;
     users.findByIdAndUpdate(id, { $set: req.body }, (err) => {
       if (err) return res.status(404).send('ID not found');
-      res.status(200).json({ message: 'Conta atualizada com suscesso' });
+      return res.status(200).json({ message: 'Conta atualizada com suscesso' });
     });
   };
 }
